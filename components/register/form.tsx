7c235d1bb851b8a51c80f9d22bf48f5fb3b2f57d
Nguyen Van Duc <ducbe2k2@gmail.com>
2022-09-20 15:46:10 +0700

    forums demo

--- components/register/form.tsx
+++ components/register/form.tsx
@@ -22,7 +22,7 @@ export function FormRegister({ registerStatus }: FormRegisterProps) {
   } = useForm<DataRegisterProps>()
   const [loading, setLoading] = useState(false)
   const router = useRouter()
-  let avatar = `<svg  fill="none" role="img" xmlns="http://www.w3.org/2000/svg"><title>Margaret Brent</title><mask id="mask__beam" maskUnits="userSpaceOnUse" x="0" y="0" width="36" height="36"><rect width="36" height="36" rx="72" fill="#FFFFFF"></rect></mask><g mask="url(#mask__beam)"><rect width="36" height="36" fill="#fc6e3d"></rect><rect x="0" y="0" width="36" height="36" transform="translate(6 6) rotate(356 18 18) scale(1.2)" fill="#0074b4" rx="6"></rect><g transform="translate(4 1) rotate(6 18 18)"><path d="M13,21 a1,0.75 0 0,0 10,0" fill="#FFFFFF"></path><rect x="13" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#FFFFFF"></rect><rect x="21" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#FFFFFF"></rect></g></g></svg>`
+  let avatar = `<svg  fill="none" style="width: 100%; height:auto;" role="img" xmlns="http://www.w3.org/2000/svg"><title>Margaret Brent</title><mask id="mask__beam" maskUnits="userSpaceOnUse" x="0" y="0" ><rect rx="72" fill="#FFFFFF"></rect></mask><g mask="url(#mask__beam)"><rect  fill="#fc6e3d"></rect><rect x="0" y="0"  transform="translate(6 6) rotate(356 18 18) scale(1.2)" fill="#0074b4" rx="6"></rect><g transform="translate(4 1) rotate(6 18 18)"><path d="M13,21 a1,0.75 0 0,0 10,0" fill="#FFFFFF"></path><rect x="13" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#FFFFFF"></rect><rect x="21" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#FFFFFF"></rect></g></g></svg>`
   const handleRegister: SubmitHandler<DataRegisterProps> = async ({
     name,
     email,
