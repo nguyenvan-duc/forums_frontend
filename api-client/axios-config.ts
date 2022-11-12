acf979dffa648cb85437acfbdccba942624b41f5
dducnv0712 <ducbe2k2@gmail.com>
2022-11-12 16:28:36 +0700

    scroll load

--- api-client/axios-config.ts
+++ api-client/axios-config.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from 'axios'
 const axiosConfig = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  // baseURL:'https://it-forums.vercel.app/api',
+  // baseURL: 'http://localhost:3000/api',
+  baseURL:'https://it-forums.vercel.app/api',
   headers: {
     'content-type': 'application/json',
   },
