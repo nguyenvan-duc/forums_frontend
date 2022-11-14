cc6bf18483d7fc8a2a8afa599f777932a0b77628
dducnv0712 <ducbe2k2@gmail.com>
2022-11-14 22:36:23 +0700

    final client render deploy

--- api-client/axios-config.ts
+++ api-client/axios-config.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from 'axios'
 const axiosConfig = axios.create({
   // baseURL: 'http://localhost:3000/api',
-  baseURL:'https://api-aptech-forums.herokuapp.com/api',
+  baseURL:'https://itcomm.onrender.com/api',
   headers: {
     'content-type': 'application/json',
   },
